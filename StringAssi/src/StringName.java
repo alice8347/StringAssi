33d989492f0c673f3c802bf1bbd66dde13a25d43
alice8347 <alice8347@hotmail.com>
2015-08-28 09:27:10 -0400

    First Commit

---
+++ StringAssi/src/StringName.java
@@ -0,0 +1,10 @@
+
+public class StringName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myName = "Yu-Jen (Alice) Lin";
+		System.out.println(myName);
+	}
+
+}
